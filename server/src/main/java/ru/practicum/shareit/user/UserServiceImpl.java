a452d5d9c2704b10f2c965e3d61cd66fbfb77ce5
annatarusova <annabelemenko1@gmail.com>
2025-07-17 22:58:39 +0300

    Спринт 16, перенёсен первый контроллер UserController

---
+++ server/src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exceptions.ConflictException;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User saveUser(User user) {
+        Optional<User> existingUser = repository.findUserByEmail(user.getEmail());
+        if (existingUser.isPresent() && !Objects.equals(existingUser.get().getId(), user.getId())) {
+            throw new ConflictException("Пользователь с таким email уже есть");
+        }
+        return repository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUser(long id) {
+
+        return repository.findById(id);
+    }
+
+    @Override
+    public User createUser(User user) {
+        user.setId(null);
+        Optional<User> existingUser = repository.findUserByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new ConflictException("Пользователь с таким email существует");
+        }
+        return repository.save(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        repository.deleteById(id);
+    }
+}
